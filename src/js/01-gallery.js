e4a443ddc842b6ab270d0b8030a2792b49b8b787
MykhailoVasyliuk <110117416+MykhailoVasyliuk@users.noreply.github.com>
2022-12-21 16:21:22 +0200

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,4 +3,52 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
-console.log(SimpleLightbox);
+const galleryContainer = document.querySelector(".gallery");
+
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a href="" class="gallery__link">
+                <img
+                    class="gallery__image" 
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    }).join("");
+ 
+}
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+
+    const isImageEl = evt.target.classList.contains("gallery__image");
+    if (!isImageEl) {
+        return;
+    }
+
+    const selectedImage = evt.target.dataset.source;
+    
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="1200" height="800">`)
+    instance.show()
+    
+    galleryContainer.addEventListener("keydown",onModalWindowClose)
+    
+    function onModalWindowClose(evt) {
+        if (evt.code === "Escape") {
+            instance.close()
+            evt.currentTarget.removeEventListener("keydown",onModalWindowClose)
+        }
+    }
+}
